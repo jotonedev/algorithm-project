f40e4087f11b9253948dc63efcef103d543d9587
John Toniutti <john.toniutti@gmail.com>
2024-12-10 16:11:37 +0100

    Improve benchmark script

--- benchmark.py
+++ benchmark.py
@@ -5,14 +5,19 @@ import logging
 import subprocess
 import sys
 import timeit
+from statistics import median
+
+import psutil
+import tqdm
+
 from dataclasses import dataclass
 from math import exp, log
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import psutil
-import tqdm
+import seaborn as sns
+from numpy.ma.extras import average
 
 
 @dataclass
@@ -30,11 +35,16 @@ def setup_logger(verbose: bool) -> None:
     :param verbose: If True, set the log level to DEBUG, otherwise set it to INFO
     """
     log_level = logging.DEBUG if verbose else logging.INFO
+    handler = logging.StreamHandler()
+    handler.setStream(tqdm.tqdm)  # <-- important
     logging.basicConfig(
         level=log_level,
         format='[%(asctime)s] [%(levelname)s] %(message)s',  # change how logs are displayed
-        datefmt='%Y-%m-%d %H:%M:%S'
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[handler]
     )
+    # disable seaborn logs
+    logging.getLogger("matplotlib").setLevel(logging.ERROR)
 
 
 def generate_input_data(length: int, min_val: int, max_val: int, *, use_float: bool = False) -> np.ndarray:
@@ -124,12 +134,14 @@ def is_sorted(data: np.ndarray) -> bool:
         if data[elem] < data[elem - 1]:
             return False
 
+
 def benchmark_algorithm(
         algorithm: Algorithm, 
-        repetitions: int = 3,
+        repetitions: int = 5,
         samples: int = 100,
         min_val: int = 100,
-        max_val: int = 1000,
+        max_val: int | None = None,
+        linear: bool = False,
 ) -> pd.DataFrame:
     """
     Benchmark the algorithm with random input data
@@ -139,6 +151,7 @@ def benchmark_algorithm(
     :param samples: The number of samples to run
     :param min_val: The minimum value of the input data
     :param max_val: The maximum value of the input data.
+    :param linear: If True, use linear scaling, otherwise use exponential scaling
     :return: A list of execution times
     """
     logging.info(f"Benchmarking algorithm {algorithm.name}")
@@ -146,41 +159,112 @@ def benchmark_algorithm(
     # Create a DataFrame to store the results of execution times in nanoseconds
     results = pd.DataFrame(
         {
-            "Input Size": pd.Series(dtype="int"),
-            "Execution Time (s)": pd.Series(dtype="float"),
+            "size": pd.Series(dtype="int"),
+            "time": pd.Series(dtype="float"),
         },
-        index=range(1, samples),
+        index=range(samples),
     )
 
-    data_length = min_val
-    scaling_factor = exp(log(max_val / min_val) / samples)
-    for i in tqdm.tqdm(range(samples+1), desc=f"Benchmarking {algorithm.name}", dynamic_ncols=True):
-        data_length = int(data_length * (scaling_factor ** i))
+    if linear:
+        data_length = 0
+        scaling_factor = int((max_val - min_val) / samples)
+    else:
+        data_length = min_val
+        scaling_factor = exp(log(max_val / min_val) / samples)
+
+    for i in tqdm.tqdm(range(samples), desc=f"Benchmarking {algorithm.name}", dynamic_ncols=True):
+        if linear:
+            data_length += scaling_factor
+        else:
+            data_length = int(min_val * (scaling_factor ** i))
 
         gc.collect()
         gc.disable()
-        data = generate_input_data(data_length, min_val, max_val)
-        execution_time = timeit.timeit(
-            stmt=lambda : algorithm.function(data, len(data)),
-            number=repetitions,
-            globals=globals()
-        )
+        execution_time = timeit.Timer(
+            stmt="algorithm.function(data, data_length)",
+            setup="data = generate_input_data(data_length, min_val, max_val)",
+            globals=globals() | locals()
+        ).repeat(repeat=repetitions, number=1)
         gc.enable()
 
-        # if it's first iteration, check the correctness of the algorithm
-        # and ignore the execution time
-        if i == 0:
-            if is_sorted(data):
-                logging.error(f"Algorithm {algorithm.name} failed the correctness check")
-                raise ValueError("Algorithm failed the correctness check")
-            continue
-
         results.loc[i] = {
-            "Input Size": data_length,
-            "Execution Time (s)": execution_time / repetitions,
+            "size": data_length,
+            "time": median(execution_time)
         }
 
     return results
+
+
+def plot_results(data: pd.DataFrame, output: Path, linear: bool = False) -> None:
+    """
+    Plot the benchmark results
+
+    :param data: The benchmark results as a DataFrame
+    :param output: The output file path
+    :param linear: If True, use linear scaling, otherwise use exponential scaling for both axes
+    :param name: The name of the output plot file
+    """
+    # cleanup the data
+    data["time"] = data["time"] * 1e3
+
+    # prepare the plot
+    sns.set_theme(style="whitegrid")
+    plot = sns.relplot(
+        data=data,
+        x="size",
+        y="time",
+        kind="line",
+        size=5,
+        aspect=1,
+        legend=False
+    )
+    plot.set_xlabels("Input Size")
+    plot.set_ylabels("Execution Time (ms)")
+
+    # add red line for quadratic function
+    plot.ax.plot(
+        data["size"],
+        (data["size"] ** 2) * 9e-7,
+        color="red",
+        linestyle="--",
+        label="O(n^2)",
+        alpha=0.8,
+        linewidth=1.5
+    )
+
+    # add orange line for n*log(n) function
+    y = np.multiply(data["size"], np.log10(data["size"])) * 8e-5
+    plot.ax.plot(
+        data["size"],
+        y,
+        color="orange",
+        linestyle="--",
+        label="O(n*log(n))",
+        alpha=0.8,
+        linewidth=1.5
+    )
+
+    # add green line for linear function
+    plot.ax.plot(
+        data["size"],
+        data["size"] * 1e-4,
+        color="green",
+        linestyle="--",
+        label="O(n)",
+        alpha=0.8,
+        linewidth=1.5
+    )
+
+    # set scale
+    if linear:
+        plot.set(xscale="linear", yscale="linear")
+    else:
+        plot.set(xscale="log", yscale="log")
+
+    # add legend on the bottom left outside the plot
+    plot.ax.legend(loc='lower left', ncol=4, frameon=False, bbox_to_anchor=(0, -0.05), fancybox=True)
+
+    plot.savefig(output, format="svg", transparent=False)
 
 
 def main(
@@ -191,16 +275,27 @@ def main(
     min_val: int,
     max_val: int | None,
     verbose: bool,
+    linear: bool = False,
 ):
     """Run the benchmarking tool"""
     setup_logger(verbose)
 
     # Increase process priority to avoid interruptions
     # check if is windows
+    process = psutil.Process()
     if sys.platform == 'win32':
-        psutil.Process().nice(psutil.HIGH_PRIORITY_CLASS)
+        process.nice(psutil.HIGH_PRIORITY_CLASS)
     elif sys.platform == 'linux':
-        psutil.Process().nice(1)
+        # set the process to the highest priority if permission is granted
+        try:
+            process.nice(-20)
+        except psutil.AccessDenied:
+            logging.warning("Could not set the process to the highest priority")
+        finally:
+            # set the process to the highest possible priority if permission is not granted
+            process.nice(1)
+    # pin process to the last core
+    process.cpu_affinity([psutil.cpu_count(logical=False) - 1])
 
     # Create an output directory if it does not exist
     output.mkdir(parents=True, exist_ok=True)
@@ -219,6 +314,7 @@ def main(
                 samples=samples,
                 min_val=min_val,
                 max_val=max_val,
+                linear=linear
             )
         except KeyboardInterrupt:
             logging.error("Benchmarking tool was interrupted")
@@ -226,6 +322,8 @@ def main(
 
         # Save the results to a CSV file
         results.to_csv(output / f"{algorithm.name}.csv", index=False)
+        # Plot the results
+        plot_results(results, output / f"{algorithm.name}.svg", linear)
 
     logging.info("Benchmarking tool completed successfully")
 
@@ -258,7 +356,7 @@ if __name__ == '__main__':
         "-r", "--repetitions",
         help="Number of repetitions for each input length to run",
         type=int,
-        default=3
+        default=5
     )
     parser.add_argument(
         "--min",
@@ -278,6 +376,12 @@ if __name__ == '__main__':
         action="store_true",
         default=False
     )
+    parser.add_argument(
+        "--linear",
+        help="Use linear scaling instead of exponential",
+        action="store_true",
+        default=False
+    )
 
     args = parser.parse_args()
 
@@ -288,5 +392,6 @@ if __name__ == '__main__':
         repetitions=args.repetitions,
         min_val=args.min,
         max_val=args.max,
-        verbose=args.verbose
+        verbose=args.verbose,
+        linear=args.linear
     )
