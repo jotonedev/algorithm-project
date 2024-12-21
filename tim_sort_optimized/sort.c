12b3f057e0d0b21764b396e188864998f840be69
John Toniutti <john.toniutti@gmail.com>
2024-12-21 10:05:46 +0100

    Add galloping mode to optimized tim sort

--- tim_sort_optimized/sort.c
+++ tim_sort_optimized/sort.c
@@ -22,8 +22,114 @@ void insertion_sort(int arr[], int left, int right) {
     }
 }
 
-// Function to merge two sorted subarrays.
-void merge(int arr[], int left, int mid, int right, int* temp_arr) {
+// Function to perform a galloping search to the left.
+// This function helps to find the position where an element from the right
+// subarray should be inserted into the left subarray during merging,
+// when galloping mode is active.
+// Returns the index in the left subarray.
+int gallop_left(int key, int arr[], int start, int length, int hint) {
+    int ofs = 1;
+    int last_ofs = 0;
+
+    if (key > arr[start + hint]) { // key could be after hint
+        int max_ofs = length - hint;
+        while (ofs < max_ofs && key > arr[start + hint + ofs]) {
+            last_ofs = ofs;
+            ofs = (ofs << 1) + 1;
+            if (ofs <= 0) {
+                ofs = max_ofs; // Overflow
+            }
+        }
+        if (ofs > max_ofs) {
+            ofs = max_ofs;
+        }
+        // Now we know that key belongs somewhere between start + hint + last_ofs and start + hint + ofs
+        last_ofs += hint;
+        ofs += hint;
+    } else { // key must be before hint
+        int max_ofs = hint + 1;
+        while (ofs < max_ofs && key <= arr[start + hint - ofs]) {
+            last_ofs = ofs;
+            ofs = (ofs << 1) + 1;
+            if (ofs <= 0) {
+                ofs = max_ofs; // Overflow
+            }
+        }
+        if (ofs > max_ofs) {
+            ofs = max_ofs;
+        }
+        // Now we know that key belongs somewhere between start + hint - ofs and start + hint - last_ofs
+        int tmp = last_ofs;
+        last_ofs = hint - ofs;
+        ofs = hint - tmp;
+    }
+
+    // Now do a binary search to find the exact location
+    last_ofs++;
+    while (last_ofs < ofs) {
+        int m = last_ofs + ((ofs - last_ofs) >> 1);
+        if (key > arr[start + m]) {
+            last_ofs = m + 1;
+        } else {
+            ofs = m;
+        }
+    }
+    return ofs; // Index in arr
+}
+
+// Function to perform a galloping search to the right.
+// Similar to gallop_left, but finds the position where an element from the
+// left subarray should be inserted into the right subarray.
+// Returns the index in the right subarray.
+int gallop_right(int key, int arr[], int start, int length, int hint) {
+    int ofs = 1;
+    int last_ofs = 0;
+
+    if (key < arr[start + hint]) { // key could be before hint
+        int max_ofs = hint + 1;
+        while (ofs < max_ofs && key < arr[start + hint - ofs]) {
+            last_ofs = ofs;
+            ofs = (ofs << 1) + 1;
+            if (ofs <= 0) {
+                ofs = max_ofs;
+            }
+        }
+        if (ofs > max_ofs) {
+            ofs = max_ofs;
+        }
+        int tmp = last_ofs;
+        last_ofs = hint - ofs;
+        ofs = hint - tmp;
+    } else { // key must be after hint
+        int max_ofs = length - hint;
+        while (ofs < max_ofs && key >= arr[start + hint + ofs]) {
+            last_ofs = ofs;
+            ofs = (ofs << 1) + 1;
+            if (ofs <= 0) {
+                ofs = max_ofs;
+            }
+        }
+        if (ofs > max_ofs) {
+            ofs = max_ofs;
+        }
+        last_ofs += hint;
+        ofs += hint;
+    }
+
+    last_ofs++;
+    while (last_ofs < ofs) {
+        int m = last_ofs + ((ofs - last_ofs) >> 1);
+        if (key < arr[start + m]) {
+            ofs = m;
+        } else {
+            last_ofs = m + 1;
+        }
+    }
+    return ofs;
+}
+
+// Function to merge two sorted subarrays using galloping mode when appropriate.
+void merge(int arr[], int left, int mid, int right, int* temp_arr, int* min_gallop) {
     int len1 = mid - left + 1;
     int len2 = right - mid;
 
@@ -36,6 +142,8 @@ void merge(int arr[], int left, int mid, int right, int* temp_arr) {
     int i = 0;      // Index for the left subarray.
     int j = 0;      // Index for the right subarray.
     int k = left;   // Index for the merged subarray in 'arr'.
+
+    int gallop = *min_gallop;
 
     // Merge the two subarrays back into the original array 'arr'.
     while (i < len1 && j < len2) {
@@ -92,7 +200,7 @@ int find_run_length(int arr[], int start, int n) {
 }
 
 // Function to merge runs on the stack to maintain invariants.
-void merge_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr) {
+void merge_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr, int* min_gallop) {
     while (*stack_size > 1) {
         int n = *stack_size - 2;
         if ((n > 0 && stack[n - 1].length <= stack[n].length + stack[n + 1].length) ||
@@ -101,7 +209,7 @@ void merge_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr) {
                 n--;
             }
             merge(arr, stack[n].start, stack[n].start + stack[n].length - 1,
-                  stack[n + 1].start + stack[n + 1].length - 1, temp_arr);
+                  stack[n + 1].start + stack[n + 1].length - 1, temp_arr, min_gallop);
             stack[n].length += stack[n + 1].length;
             stack[n + 1] = stack[n + 2];
             (*stack_size)--;
@@ -113,13 +221,14 @@ void merge_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr) {
 
 // Function to merge all remaining runs on the stack.
 void merge_force_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr) {
+    int min_gallop = MIN_GALLOP;
     while (*stack_size > 1) {
         int n = *stack_size - 2;
         if (n > 0 && stack[n - 1].length < stack[n + 1].length) {
             n--;
         }
         merge(arr, stack[n].start, stack[n].start + stack[n].length - 1,
-              stack[n + 1].start + stack[n + 1].length - 1, temp_arr);
+              stack[n + 1].start + stack[n + 1].length - 1, temp_arr, &min_gallop);
         stack[n].length += stack[n + 1].length;
         (*stack_size)--;
     }
@@ -133,6 +242,7 @@ void tim_sort(int arr[], int n, int* temp_arr, Run* stack) {
 
     int minrun = calculate_minrun(n);
     int stack_size = 0;
+    int min_gallop = MIN_GALLOP;
 
     int start = 0;
     while (start < n) {
@@ -150,7 +260,7 @@ void tim_sort(int arr[], int n, int* temp_arr, Run* stack) {
         stack[stack_size].length = run_length;
         stack_size++;
 
-        merge_collapse(arr, stack, &stack_size, temp_arr);
+        merge_collapse(arr, stack, &stack_size, temp_arr, &min_gallop);
 
         start = end + 1;
     }
--- tim_sort_optimized/sort.h
+++ tim_sort_optimized/sort.h
@@ -4,6 +4,9 @@
 // Threshold for switching to insertion sort (tuned value).
 #define THRESHOLD 64
 
+// Minimum number of elements to consider for galloping mode.
+#define MIN_GALLOP 7
+
 // Initial minimum run length (tuned value).
 #define MIN_MERGE 32
 
@@ -14,11 +17,13 @@ typedef struct {
 } Run;
 
 void insertion_sort(int arr[], int left, int right);
-void merge(int arr[], int left, int mid, int right, int* temp_arr);
+int gallop_left(int key, int arr[], int start, int length, int hint);
+int gallop_right(int key, int arr[], int start, int length, int hint);
+void merge(int arr[], int left, int mid, int right, int* temp_arr, int* min_gallop);
 int calculate_minrun(int n);
 int find_run_length(int arr[], int start, int n);
-void merge_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr);
+void merge_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr, int* min_gallop) ;
 void merge_force_collapse(int arr[], Run stack[], int* stack_size, int* temp_arr);
-void tim_sort(int arr[], int n, int temp_arr[], Run stack[]);
+void tim_sort(int arr[], int n, int* temp_arr, Run* stack);
 
 #endif
